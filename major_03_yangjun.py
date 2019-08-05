a8f884aa23fc0cb45039e17c70c219f3494b5c04
yangjun <1319605058@qq.com>
2019-08-05 17:12:56 +0800

    03待修改

---
+++ major_03_yangjun.py
@@ -0,0 +1,61 @@
+# encoding=utf-8
+
+import csv
+import pandas
+import config
+import time
+# 将用户名去重,得到所有的用户列表
+def get_username():
+    csv_info= pandas.read_csv(config.url,usecols=[1])
+    csv_list=csv_info["姓名"]
+    csv_list=list(csv_list)
+    csv_set=set(csv_list)
+    user_list=list(csv_set)
+    user_new_list=[]
+    count=0
+    for user in user_list:
+        user_new_list.append([user,count])
+    # print(user_new_list)
+    return user_new_list
+
+def need_03(list):
+    reader=list(csv.reader(open(config.url,encoding="utf-8")))
+    list_user=list
+    for i in range(len(reader)):
+        for j in range(len(list_user)):
+            if reader[i][1]==list_user[j][0]:
+                list_user[j][1]+=1
+                continue
+    return list_user
+
+
+
+
+
+
+# date = 0
+# name = 1
+
+# with open("D:/kaoqin.csv/kaoqin.csv","r",encoding="utf-8") as csvfile:
+#     user = list(csv.reader(csvfile))
+#
+#     # print(user)
+#
+#     dic={}
+#     for i in range(1,len(user)):
+#         if user[i][1] not in dic:
+#             dic[user[i][1]] = user[i][0]
+#     print(dic)
+#
+#     # for a,b in dic.items():
+#     #     print(a,"\t",b,"\t")
+
+
+
+
+
+
+
+
+
+
