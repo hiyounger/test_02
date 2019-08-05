cd7a776c9c4174935c872394d814e23d31d10814
Liuhebo <1041135120@qq.com>
2019-08-05 15:57:30 +0800

    liu

---
+++ liu_count.py
@@ -0,0 +1,16 @@
+#encoding:utf-8
+import csv
+import config
+def need_02():
+    with open(config.url, 'r', encoding="utf-8") as mycsvfile:
+        data = list(csv.reader(mycsvfile))
+        data.pop(0)
+    dic = {}
+    for i in range(len(data)):
+        if data[i][10] != "未打卡":
+            if data[i][1] not in dic:
+                dic[data[i][1]] = 1
+            else:
+                dic[data[i][1]] += 1
+    for a, b in dic.items():
+        return (a, "打卡", b, "次")
